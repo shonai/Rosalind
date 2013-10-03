ecadcbc1c63e2fc114b4fa55fd0d74b681097e3c
shonai <knight2010@yandex.ru>
2013-10-03 22:48:53 +0400

    Transcribing DNA into RNA
    Working version of the named problem.

--- Transcribing_DNA_into_RNA/Transcribing_DNA_into_RNA/main.cpp
+++ Transcribing_DNA_into_RNA/Transcribing_DNA_into_RNA/main.cpp
@@ -8,32 +8,31 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
     ifstream FileToBeOpen;
     ofstream OutputFile;
         string FileName;
         string StrFromFile;
 
+
         cout<< "Please, input the name of a file, containing DNA"<<endl;
         cin>>FileName;
         FileToBeOpen.open(FileName.c_str(), ifstream::in);
-
         FileToBeOpen >> StrFromFile;
-
         FileToBeOpen.close();
-        // TODO: probably, loop can be used to
-        // go through all the string, replacing
-        // symbols, using string::replace
-        // with pos = string.find(symbol)
 
-        for(string::const_iterator iter = StrFromFile.begin(); iter != StrFromFile.end(); iter++)
-            if (*iter == 'T')
-                StrFromFile.replace(iter,1, 'U');
-        cout << StrFromFile;
 
         OutputFile.open("Output.txt");
-        OutputFile << StrFromFile;
+        for(string::const_iterator iter = StrFromFile.begin(); iter != StrFromFile.end(); iter++)
+            if (*iter == 'T')
+            {
+                OutputFile<<'U';
+            }
+            else
+            {
+                OutputFile<<*iter;
+            }
         OutputFile.close();
+
 
     return a.exec();
 }
