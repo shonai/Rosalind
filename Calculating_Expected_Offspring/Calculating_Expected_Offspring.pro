836e21eb81a14ecd9d0ce6bbc3a3ab8727731317
shonai <knight2010@yandex.ru>
2013-10-10 11:25:41 +0400

    Completed: Calculating expected offspring
    Named problem solved. Have some issues with output, cout cant output big
    doubles normally O_o

---
+++ Calculating_Expected_Offspring/Calculating_Expected_Offspring.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-10-10T10:59:04
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Calculating_Expected_Offspring
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ Calculating_Expected_Offspring/main.cpp
@@ -0,0 +1,36 @@
+#include <QtCore/QCoreApplication>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    long AA_AA;
+    long AA_Aa;
+    long AA_aa;
+    long Aa_Aa;
+    long Aa_aa;
+    long aa_aa;
+
+    cout << "Please, input the count of AA-AA couples: ";
+    cin  >> AA_AA;
+    cout << "Please, input the count of AA-Aa couples: ";
+    cin  >> AA_Aa;
+    cout << "Please, input the count of AA-aa couples: ";
+    cin  >> AA_aa;
+    cout << "Please, input the count of Aa-Aa couples: ";
+    cin  >> Aa_Aa;
+    cout << "Please, input the count of Aa-aa couples: ";
+    cin  >> Aa_aa;
+    cout << "Please, input the count of aa-aa couples: ";
+    cin  >> aa_aa;
+
+    double result = Aa_Aa * 1.5 + AA_AA * 2. + AA_Aa * 2. + AA_aa * 2. + Aa_aa;
+//    cout << result;
+    printf("%f", result);
+
+    return a.exec();
+}
